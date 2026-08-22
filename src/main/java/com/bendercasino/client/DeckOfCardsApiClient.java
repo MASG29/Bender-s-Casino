7000c4e65cb939e870882cae56d47fd6343b6f23
diogompintoo <diogomartinsp1992@gmail.com>
2026-08-22 22:39:15 +0100

    feat: add DeckOfCardsApi, and RestClientConfig [B3]

--- src/main/java/com/bendercasino/client/DeckOfCardsApiClient.java
+++ src/main/java/com/bendercasino/client/DeckOfCardsApiClient.java
@@ -1,8 +1,11 @@
 package com.bendercasino.client;
 
+import com.bendercasino.client.dto.DeckResponse;
+import com.bendercasino.client.dto.DrawResponse;
 import com.bendercasino.exception.DeckApiException;
 import com.bendercasino.model.Card;
 import com.bendercasino.model.Deck;
+import com.bendercasino.util.CardMapper;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
@@ -24,13 +27,43 @@ public class DeckOfCardsApiClient implements DeckClient {
 
     @Override
     public Deck newShuffledDeck(int deckCount) {
-        // TODO — GET /new/shuffle/?deck_count={deckCount}
-        throw new UnsupportedOperationException("implement");
+
+        try {
+            DeckResponse response = restClient
+                    .get()
+                    .uri("/new/shuffle/?deck_count={n}", deckCount)
+                    .retrieve()
+                    .body(DeckResponse.class);
+
+            if (response == null || !response.success()){
+                throw new DeckApiException("Fail to create deck");
+            }
+            return new Deck(response.deck_id(), response.remaining());
+        }catch (RestClientException e) {
+            throw new DeckApiException("Fail to create deck");
+        }
     }
+
 
     @Override
     public List<Card> draw(String deckId, int count) {
         // TODO — GET /{deckId}/draw/?count={count}
-        throw new UnsupportedOperationException("implement");
+        try {
+            DrawResponse response = restClient
+                    .get()
+                    .uri("/{id}/draw/?count={n}", deckId, count)
+                    .retrieve()
+                    .body(DrawResponse.class);
+
+            if (response == null || !response.success()){
+                throw new DeckApiException("Fail to draw deck");
+            }
+            return response.cards()
+                    .stream()
+                    .map(CardMapper::toDomain)
+                    .toList();
+        }catch (RestClientException e) {
+            throw new DeckApiException("Fail to draw deck");
+        }
     }
 }
