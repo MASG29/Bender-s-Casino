a97b83d50d7ed122c19f79746c62871d72cf76d3
diogompintoo <diogomartinsp1992@gmail.com>
2026-08-23 00:28:36 +0100

    feat: add BlackjackController and PlayerController (DEALER_BUST) [B8]

--- src/main/java/com/bendercasino/controller/BlackjackController.java
+++ src/main/java/com/bendercasino/controller/BlackjackController.java
@@ -1,7 +1,14 @@
 package com.bendercasino.controller;
 
 import com.bendercasino.dto.*;
+import com.bendercasino.model.GameSession;
+import com.bendercasino.model.GameStatus;
+import com.bendercasino.model.JokeTrigger;
+import com.bendercasino.model.Player;
 import com.bendercasino.service.BlackjackService;
+import com.bendercasino.service.JokeService;
+import com.bendercasino.service.PlayerService;
+import com.bendercasino.util.CardMapper;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
@@ -11,38 +18,64 @@ import java.util.UUID;
 public class BlackjackController {
 
     private final BlackjackService blackjackService;
+    private final PlayerService playerService;
+    private final JokeService jokeService;
 
-    public BlackjackController(BlackjackService blackjackService) {
+    public BlackjackController(BlackjackService blackjackService, PlayerService playerService, JokeService jokeService) {
         this.blackjackService = blackjackService;
+        this.playerService = playerService;
+        this.jokeService = jokeService;
     }
 
     @PostMapping("/start")
     public GameStateResponse start(@Valid @RequestBody StartGameRequest request) {
-        // TODO
-        throw new UnsupportedOperationException("implement");
+        GameSession session = blackjackService.start(request.playerId(), request.bet());
+        return toDTO(session);
     }
 
     @PostMapping("/hit")
     public GameStateResponse hit(@Valid @RequestBody PlayerActionRequest request) {
-        // TODO
-        throw new UnsupportedOperationException("implement");
+        GameSession session = blackjackService.hit(request.playerId());
+        return toDTO(session);
     }
 
     @PostMapping("/stand")
     public GameStateResponse stand(@Valid @RequestBody PlayerActionRequest request) {
-        // TODO
-        throw new UnsupportedOperationException("implement");
+        GameSession session = blackjackService.stand(request.playerId());
+        return toDTO(session);
     }
 
     @GetMapping("/state/{playerId}")
     public GameStateResponse state(@PathVariable UUID playerId) {
-        // TODO
-        throw new UnsupportedOperationException("implement");
+        GameSession session = blackjackService.getState(playerId);
+        return toDTO(session);
     }
 
     @PostMapping("/joke")
     public JokeResponse joke(@Valid @RequestBody JokeRequest request) {
-        // TODO
-        throw new UnsupportedOperationException("implement");
+        Player player = playerService.findById(request.playerId());
+        JokeTrigger trigger = JokeTrigger.valueOf(request.trigger());
+        String joke = jokeService.jokeFor(player, trigger);
+        return new JokeResponse(joke);
+    }
+
+    private GameStateResponse toDTO(GameSession session) {
+        boolean hidden = session.getStatus() == GameStatus.PLAYER_TURN;
+        HandDto dealerDto = hidden
+                ? CardMapper.toDtoDealerHidden(session.getDealerHand())
+                : CardMapper.toDto(session.getDealerHand());
+
+        return new GameStateResponse(
+                session.getGameId(),
+                session.getPlayerId(),
+                session.getStatus().name(),
+                CardMapper.toDto(session.getPlayerHand()),
+                dealerDto,
+                session.getBet().amount(),
+                session.getOutcome() != null ? session.getOutcome().name() : null,
+                session.getBet().payout(),
+                null,
+                null
+        );
     }
 }
--- src/main/java/com/bendercasino/controller/PlayerController.java
+++ src/main/java/com/bendercasino/controller/PlayerController.java
@@ -2,6 +2,7 @@ package com.bendercasino.controller;
 
 import com.bendercasino.dto.CreatePlayerRequest;
 import com.bendercasino.dto.PlayerResponse;
+import com.bendercasino.model.Player;
 import com.bendercasino.service.PlayerService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -22,25 +23,37 @@ public class PlayerController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public PlayerResponse create(@Valid @RequestBody CreatePlayerRequest request) {
-        // TODO
-        throw new UnsupportedOperationException("implement");
+        Player player = playerService.create(request.name());
+        return toDto(player);
     }
 
     @GetMapping("/{id}")
     public PlayerResponse getById(@PathVariable UUID id) {
-        // TODO
-        throw new UnsupportedOperationException("implement");
+        return toDto(playerService.findById(id));
     }
 
     @GetMapping("/{id}/balance")
     public Map<String, Integer> getBalance(@PathVariable UUID id) {
-        // TODO
-        throw new UnsupportedOperationException("implement");
+        Player player = playerService.findById(id);
+        return Map.of("balance", player.getBalance());
     }
 
     @PostMapping("/{id}/reset")
     public PlayerResponse reset(@PathVariable UUID id) {
-        // TODO
-        throw new UnsupportedOperationException("implement");
+        return toDto(playerService.reset(id));
+    }
+
+    private PlayerResponse toDto(Player player) {
+        return new PlayerResponse(
+                player.getId(),
+                player.getName(),
+                player.getBalance(),
+                new PlayerResponse.StatsDto(
+                        player.getTotalWins(),
+                        player.getTotalLosses(),
+                        player.getTotalPushes(),
+                        player.getTotalBlackjacks()
+                )
+        );
     }
 }
