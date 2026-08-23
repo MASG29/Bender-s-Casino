d8810ad02d9fc724f6e6eddc64881ea8dadc655e
Eddie-PL <alteduardopaulino@gmail.com>
2026-08-23 23:25:39 +0100

    Routes completed, respective .js done but only a skeleton navigation is possible

---
+++ src/main/resources/static/views/pages/blackjack.js
@@ -0,0 +1,7 @@
+export function init() {
+    document.querySelector("main").innerHTML = `
+        <section class="join">
+            <h2>Blackjack</h2>
+        </section>
+    `;
+}
---
+++ src/main/resources/static/views/pages/profile.js
@@ -0,0 +1,7 @@
+export function init() {
+    document.querySelector("main").innerHTML = `
+        <section class="join">
+            <h2>Profile</h2>
+        </section>
+    `;
+}
