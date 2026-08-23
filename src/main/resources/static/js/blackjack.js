df7b3b2d5d2d3c785d22f93b57331b0fd98aa81f
AquaTPPT <paulostiago717@gmail.com>
2026-08-23 19:33:03 +0100

    Added the 'hit' and 'stand' buttons for the Blackjack UI. Added basic assets for the cards and the table (WIP).

--- src/main/resources/static/js/blackjack.js
+++ src/main/resources/static/js/blackjack.js
@@ -1,7 +1,13 @@
-function init() {
-    // ...
+import { element, button, stylizedButton } from "./constants/element.js";
+
+const body = document.querySelector("#App");
+
+export function render() {
+  const ui = element("div", body, ["blackjack-ui"]);
+  createUiButtons(ui);
 }
 
-function createButtons() {
-    // ...
+function createUiButtons(parent) {
+  const hit = stylizedButton(parent, "Hit");
+  const stand = stylizedButton(parent, "Stand");
 }
--- src/main/resources/static/js/constants/element.js
+++ src/main/resources/static/js/constants/element.js
@@ -1,11 +1,23 @@
-
-export function element(type, parent, classes) {
-    const el = document.createElement(type);
-    if (classes != undefined) {
-        for (const style of classes) {
-            el.classList.add(style);
-        }
+export function element(type, parent, classes, text = "") {
+  const el = document.createElement(type);
+  if (classes != undefined) {
+    for (const style of classes) {
+      el.classList.add(style);
     }
-    parent.appendChild(el);
-    return el
+  }
+  el.textContent = text;
+  parent.appendChild(el);
+  return el;
+}
+
+export function button(parent, text, classes) {
+  const button = element("button", parent, classes, text);
+  button.textContent = text;
+  return button;
+}
+
+export function stylizedButton(parent, text) {
+  const but = button(parent);
+  const span = element("span", but, ["button_top"], text);
+  return button;
 }
