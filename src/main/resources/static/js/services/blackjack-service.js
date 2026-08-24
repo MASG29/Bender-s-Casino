48dc48551a868afec3e59460f839c1e7564171de
AquaTPPT <paulostiago717@gmail.com>
2026-08-24 14:17:09 +0100

    Basic api has been completed.

--- src/main/resources/static/js/services/blackjack-service.js
+++ src/main/resources/static/js/services/blackjack-service.js
@@ -1,5 +1,65 @@
 import { API_BASE_URL } from "../constants/utils.js";
 
-export async function startGame() {
-  const response = await fetch(`${API_BASE_URL}`); //...
+export async function startGame(playerId, betAmount) {
+  const response = await fetch(`${API_BASE_URL}blackjack/start`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      playerId: playerId,
+      bet: betAmount,
+    }),
+  });
+
+  return response.json();
+}
+
+export async function hit(playerId) {
+  const response = await fetch(`${API_BASE_URL}blackjack/hit`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      playerId: playerId,
+    }),
+  });
+
+  if (!response.ok) {
+    const errMessage = await response.text();
+    throw new Error(errMessage);
+  }
+
+  return response.json();
+}
+
+export async function stand(playerId) {
+  const response = await fetch(`${API_BASE_URL}blackjack/stand`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      playerId: playerId,
+    }),
+  });
+
+  if (!response.ok) {
+    const errMessage = await response.text();
+    throw new Error(errMessage);
+  }
+
+  return response.json();
+}
+
+export async function getState(playerId) {
+  const response = await fetch(`${API_BASE_URL}blackjack/state/${playerId}`);
+
+  if (!response.ok) {
+    const errMessage = await response.text();
+    throw new Error(errMessage);
+  }
+
+  return response.json();
 }
