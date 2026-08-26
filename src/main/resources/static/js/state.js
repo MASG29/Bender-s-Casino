20e982b827653577b035bf816f72a31caddcc466
diogompintoo <diogomartinsp1992@gmail.com>
2026-08-26 01:00:18 +0100

    feat: create state.js for centralize session insted calling sessionStorage all the time

---
+++ src/main/resources/static/js/state.js
@@ -0,0 +1,27 @@
+const state = {
+    getPlayer() {
+        return {
+            playerId    : sessionStorage.getItem("playerId"),
+            playerName  : sessionStorage.getItem("playerName"),
+            balance     : sessionStorage.getItem("balance"),
+        };
+    },
+
+    setPlayer(player) {
+        sessionStorage.setItem("playerId",   player.playerId);
+        sessionStorage.setItem("playerName", player.name);
+        sessionStorage.setItem("balance",    player.balance);
+    },
+
+    clearPlayer() {
+        sessionStorage.removeItem("playerId");
+        sessionStorage.removeItem("playerName");
+        sessionStorage.removeItem("balance");
+    },
+
+    isLoggedIn() {
+        return !!sessionStorage.getItem("playerId");
+    }
+}
+
+export default state;
