2b9d1e599c6f9f66692e3ff114c094ae6615b9a7
diogompintoo <diogomartinsp1992@gmail.com>
2026-08-26 03:34:36 +0100

    integration login backend with login frontend, ned to fix minor issius

--- src/main/java/com/bendercasino/dto/CreatePlayerRequest.java
+++ src/main/java/com/bendercasino/dto/CreatePlayerRequest.java
@@ -4,6 +4,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CreatePlayerRequest(
-        @NotBlank String name,
-        @NotBlank @Size(min = 3, max = 30) String username,
-        @NotBlank @Size(min = 4, max = 50) String password) {}
+    @NotBlank String name,
+    @NotBlank String firstName,
+    @NotBlank String lastName,
+    @NotBlank String email,
+    @NotBlank @Size(min = 6) String password
+) {}
