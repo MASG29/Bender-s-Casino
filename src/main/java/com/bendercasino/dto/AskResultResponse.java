b8a865978aab390a1a5f5779e6f4dbbd97b3580d
diogompintoo <diogomartinsp1992@gmail.com>
2026-08-28 23:35:07 +0100

    feat: creating Dto`s for logic of the game

---
+++ src/main/java/com/bendercasino/dto/AskResultResponse.java
@@ -0,0 +1,13 @@
+package com.bendercasino.dto;
+
+import java.util.List;
+
+public record AskResultResponse(
+        boolean gotCards,
+        List<CardDto> cardsReceived,
+        boolean drewFromDeck,
+        CardDto drawnCard,
+        boolean formedBook,
+        String message,
+        PeixinhoStateResponse gameState
+) {}
