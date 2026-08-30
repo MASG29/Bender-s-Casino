d2af8908c9bbc6079bbcd1cb9f1e13958a86c4a9
AquaTPPT <195988559+AquaTPPT@users.noreply.github.com>
2026-08-30 20:45:22 +0100

    Added the rest of the structure. EventListeners still missing.

--- src/main/resources/static/views/pages/videopoker.js
+++ src/main/resources/static/views/pages/videopoker.js
@@ -1,5 +1,7 @@
 import router from "../../router.js";
 import { CHIPS } from "../../js/blackjack/chips.js";
+import { element, button, stylizedButton } from "../../js/constants/element.js";
+import { PAYOUTS } from "../../js/videopoker/payouts.js";
 
 export function init() {
   const main = document.querySelector("main");
@@ -7,7 +9,7 @@ export function init() {
   main.innerHTML = `
 
     <section class="bj">
-      <h2>Blackjack</h2>
+      <h2>Video Poker</h2>
         <div class="bj-floor">
           <div id="chips" class="bj-chips">
             ${CHIPS.map(
@@ -18,38 +20,56 @@ export function init() {
             `,
             ).join("")}
     </div>
-
-    <div id="arcade">
-        <div id="table">
-          <div class="bj-deck" id="bj-deck">
-            <div class="bj-deck-card"></div>
-            <div class="bj-deck-card"></div>
-            <div class="bj-deck-card"></div>
-            <div class="bj-deck-card"></div>
-            <div class="bj-deck-card"></div>
-          </div>
-          <div id= "game">
+    <div class="vp-table">
+      <table id="payouts" class="payouts-table">
+            ${PAYOUTS.map(
+              (payout) => `
+              <tr>
+                <td>${payout.name}</td>
+                ${payout.payout.map((el) => `<td>${el}</td>`).join("")}
+              </tr>
+              `,
+            ).join("")}
+    </table>
+          <div id="game">
             <div id="card-slot-container">
     `;
 
   const cardSlotContainer = document.querySelector("#card-slot-container");
 
-  const cardSlot = `<div class="card-slot">
-                    </div>`;
-  for (let i = 0; i < 5; i++) {
-    cardSlotContainer.insertAdjacentHTML("beforeend", cardSlot);
-  }
-
   cardSlotContainer.insertAdjacentHTML(
     "afterend",
     `
-                  <div id="buttonContainer">
+                <div id="buttonContainer">
                 </div>
               </div>
+                <div class = "lower-button-container">
+                    <p class="amount">Bet amount: 0</p>
+                </div>
             </div>
         </div>
     </div>
     `,
   );
+
+  const buttonContainer = document.querySelector("#buttonContainer");
+  const lowerButContainer = document.querySelector(".lower-button-container");
+  const cardSlot = `<div class="bj-slot"></div>`;
+
+  const bet = stylizedButton(lowerButContainer, "Bet");
+
+  for (let i = 0; i < 5; i++) {
+    cardSlotContainer.insertAdjacentHTML("beforeend", cardSlot);
+    stylizedButton(buttonContainer, "Hold");
+  }
+
   return;
+}
+
+function getOptions() {
+  const buttonContainer = document.querySelector("#buttonContainer");
+  const pokerHand = 5;
+
+  for (let val of pokerHand) {
+  }
 }
